665320b891c15da5e63c870d1185ef39077712f5
Phillip Novicki <Phillip.Novicki@gmail.com>
2022-02-24 16:42:30 -0600

    stacys version

---
+++ alembic/versions/317e5bb66ada_stacys_version.py
@@ -0,0 +1,24 @@
+"""stacys version
+
+Revision ID: 317e5bb66ada
+Revises: 711708910f7e
+Create Date: 2022-02-24 16:41:35.292426
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '317e5bb66ada'
+down_revision = '711708910f7e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
